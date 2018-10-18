e0dd433882e60b38e7f190a6b88e66ef070d96b5
hiencoder <mrphonglinh2605@gmail.com>
2018-10-18 17:09:57 +0700

    Commit init

---
+++ app/src/main/java/com/example/hiennv/navigationsample/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hiennv.navigationsample.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hiennv.navigationsample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
